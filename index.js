49050932802dd66b9d0a67c0bc1ae7351463f2ac
my5559647-ux <my5559647@gmail.com>
2026-01-07 23:40:34 +0500

    Added user controller, routes, and MVC structure

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/User.route.js';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use('/api/user', userRouter);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
