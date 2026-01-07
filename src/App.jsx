42e1d7ac6c338c2bfc06617c3d4f2588f68e0543
my5559647-ux <my5559647@gmail.com>
2026-01-07 22:17:54 +0500

    added react router and created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-slate-900">
-      <h1 className="text-5xl font-bold text-blue-400 underline decoration-red-500">
-        Tailwind Working!
-      </h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
