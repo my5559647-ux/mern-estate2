8b1c30462748dbd612da7ee52d95bf045205b1b0
my5559647-ux <my5559647@gmail.com>
2026-01-07 22:08:54 +0500

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-900">
+      <h1 className="text-5xl font-bold text-blue-400 underline decoration-red-500">
+        Tailwind Working!
+      </h1>
+    </div>
+  )
+}
