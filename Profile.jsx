a6677d92cd9a941b717a3e990bbfc2b9bcc40001
my5559647-ux <my5559647@gmail.com>
2026-01-09 00:06:22 +0500

    Complete page listing UI

--- Profile.jsx
+++ Profile.jsx
@@ -190,10 +190,7 @@ export default function Profile() {
         </Link>
       </form>
       <div className='flex justify-between mt-5 font-semibold'>
-        <span
-          onClick={handleDeleteUser}
-          className='text-red-700 cursor-pointer'
-        >
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>
           Delete account
         </span>
         <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
@@ -217,24 +214,13 @@ export default function Profile() {
         <div className='flex flex-col gap-4'>
           <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
           {userListings.map((listing) => (
-            <div
-              key={listing._id}
-              className='border rounded-lg p-3 flex justify-between items-center gap-4'
-            >
+            <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
               <Link to={`/listing/${listing._id}`}>
-                <img
-                  src={listing.imageUrls[0]}
-                  alt='listing cover'
-                  className='h-16 w-16 object-contain'
-                />
+                <img src={listing.imageUrls[0]} alt='listing cover' className='h-16 w-16 object-contain' />
               </Link>
-              <Link
-                className='text-slate-700 font-semibold hover:underline truncate flex-1'
-                to={`/listing/${listing._id}`}
-              >
+              <Link className='text-slate-700 font-semibold hover:underline truncate flex-1' to={`/listing/${listing._id}`}>
                 <p>{listing.name}</p>
               </Link>
-
               <div className='flex flex-col items-center'>
                 <button className='text-red-700 uppercase'>Delete</button>
                 <button className='text-green-700 uppercase'>Edit</button>
